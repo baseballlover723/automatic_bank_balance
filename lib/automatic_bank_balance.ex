84ae999b9f778e93c03bc7c7058999012365eb71
Philip Ross <dev@baseballlover723.com>
2020-08-18 21:33:52 -0700

    Initial commit

---
+++ lib/automatic_bank_balance.ex
@@ -0,0 +1,18 @@
+defmodule AutomaticBankBalance do
+  @moduledoc """
+  Documentation for `AutomaticBankBalance`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutomaticBankBalance.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/automatic_bank_balance_test.exs
@@ -0,0 +1,8 @@
+defmodule AutomaticBankBalanceTest do
+  use ExUnit.Case
+  doctest AutomaticBankBalance
+
+  test "greets the world" do
+    assert AutomaticBankBalance.hello() == :world
+  end
+end
