cd0c858294557fe2df84eae2820b14eb992f56c5
Philip Ross <dev@baseballlover723.com>
2020-08-22 15:41:45 -0700

    added dialyxir dev dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule AutomaticBankBalance.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -21,6 +22,7 @@ defmodule AutomaticBankBalance.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}, # static code analysis
       {:flow, "~> 1.0"},
     ]
   end
